8bf8d3b3b94baf2b4beaf36647ed7b666dddced7
aimbesi1 <91354954+aimbesi1@users.noreply.github.com>
2022-03-03 23:49:39 -0500

    Add files via upload
    I added in three new moves. Now the player can slide, dropkick, and flip kick. Load this up and test them out yourself. Go crazy!
    I also figured out how to load the player texture properly. It turns out that the problem can be fixed with one line of code, but it really wasn't obvious to me. Either way, it's all done. Once we get all the sprites together in one spritesheet, we can easily load in all the animations through the Player class.

---
+++ src/Objects/Button.js
@@ -0,0 +1,43 @@
+// Code from https://phasertutorials.com/creating-a-phaser-3-template-part-3/
+
+import 'phaser';
+
+/**
+    Constructs a default button to go to a different game scene.
+    @param scene the scene this button appears in
+    @param x the x position of the button
+    @param y the y position of the button
+    @key1 the key of the unpressed button image
+    @key2 the key of the button image when hovered over with the mouse
+    @text the text in the button
+    @targetScene the scene to start when the button is clicked
+*/
+export default class Button extends Phaser.GameObjects.Container {
+    constructor(scene, x, y, key1, key2, text, targetScene) {
+        super(scene);
+        this.scene = scene;
+        this.x = x;
+        this.y = y;
+
+        this.button = this.scene.add.sprite(0, 0, key1).setInteractive();
+        this.text = this.scene.add.text(0, 0, text, { fontSize: '32px', fill: '#fff' });
+        Phaser.Display.Align.In.Center(this.text, this.button);
+
+        this.add(this.button);
+        this.add(this.text);
+
+        this.button.on('pointerdown', function () {
+            this.scene.scene.start(targetScene);
+        }.bind(this));
+
+        this.button.on('pointerover', function () {
+            this.button.setTexture(key2);
+        }.bind(this));
+
+        this.button.on('pointerout', function () {
+            this.button.setTexture(key1);
+        }.bind(this));
+
+        this.scene.add.existing(this);
+    }
+}
