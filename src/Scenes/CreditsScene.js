71f35e58588dbfe82632ac3eac8c73dee02c773a
Josiah Cornelius <josiahcornelius@Josiahs-MBP.attlocal.net>
2022-04-19 16:24:09 -0400

    fixed width/height for credits

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -2,6 +2,7 @@
 
 import 'phaser';
 import Button from '../Objects/Button';
+import config from '../Config/config';
 
 
 export default class CreditsScene extends Phaser.Scene {
@@ -10,11 +11,11 @@ export default class CreditsScene extends Phaser.Scene {
   }
 
   create () {
-    this.backButton = new Button(this, 100, 50, 'blueButton1', 'blueButton2', 'Back', 'Title');
-    this.credits = this.add.text(480, 100, 'Credits', { fontSize: '32px', fill: '#fff' });
+    this.backButton = new Button(this, 100, 50, 'blueButton1', 'blueButton2', 'MENU', 'Title');
+    this.credits = this.add.text(config.width/2  - 200, config.height/2 - 230,  'Credits', { fontSize: '32px', fill: '#fff' });
     this.creditsText1= this.add.text(120, 200, 'Story By\n  \nJosiah Cornelius\nTony Imbesi\nNabeeha Ashfaq\nJohanna Beatty', { fontSize: '20px', fill: '#fff' });
     this.creditsText2 = this.add.text(120, 350, 'Character/Level Art\n  \nTony Imbesi\nNabeeha Ashfaq\nJohanna Beatty\nNicoletta Imbesi(outside class)', { fontSize: '20px', fill: '#fff' });
-    this.creditsText3 = this.add.text(640, 350, 'Music/SFX\n  \nTony Imbesi\nNabeeha Ashfaq\n', { fontSize: '20px', fill: '#fff' });
-    this.creditsText4 = this.add.text(640, 180, '\nGameplay Physics/Programmers\n  \nJosiah Cornelius \nTony Imbesi\nNabeeha Ashfaq\n', { fontSize: '20px', fill: '#fff' });
+    this.creditsText3 = this.add.text(config.width/2, config.height/2 - 150, 'Music/SFX\n  \nTony Imbesi\nNabeeha Ashfaq\n', { fontSize: '20px', fill: '#fff' });
+    this.creditsText4 = this.add.text(config.width/2, config.height/2 - 30, '\nGameplay Physics/Programmers\n  \nJosiah Cornelius \nTony Imbesi\nNabeeha Ashfaq\n', { fontSize: '20px', fill: '#fff' });
   }
 };
