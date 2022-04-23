a3fe497970da5d36e6a6ecd555fc9bd5d8fb3148
Josiah Cornelius <josiahcornelius@Josiahs-MacBook-Pro.local>
2022-04-23 15:43:05 -0400

    removed more stuff

--- src/Scenes/KeybindingScene.js
+++ src/Scenes/KeybindingScene.js
@@ -1,6 +1,8 @@
 //code by Josiah
 import 'phaser';
 import Button from '../Objects/Button';
+import config from '../Config/config';
+
 
 export default class KeybindingScene extends Phaser.Scene {
   constructor () {
@@ -11,15 +13,19 @@ export default class KeybindingScene extends Phaser.Scene {
     this.controls = this.sys.game.globals.controls;
     this.controls.addControls(this);
     this.controlText = this.add.text(500, 70, 'Control Layout', { fontSize: '32px', fill: '#fff' });
-    this.backButton = new Button(this, 100, 50, 'blueButton1', 'blueButton2', 'Back', 'Title');
+    this.backButton = new Button(this, 100, 50, 'blueButton1', 'blueButton2', 'MENU', 'Title');
     this.rightControlText = this.add.text(200, 200, 'Right Key(move forward)', { fontSize: 24 });
     this.leftControText = this.add.text(200, 290, 'Left Key(move backwards)', { fontSize: 24 });
     this.jumpControlText = this.add.text(200, 380, 'Z(jump)', { fontSize: 24 });
     this.attackControText = this.add.text(200, 470, 'X(attack)', { fontSize: 24 });
-    this.newRightControl = this.add.text(700, 200, 'Set button',  { fontSize: 24, color:'blue'});
-    this.newLeftControl = this.add.text(700, 290, 'Set button',   { fontSize: 24, color:'blue'});
-    this.newJumpControl = this.add.text(700, 380, 'Set button',  { fontSize: 24,   color:'blue'});
-    this.newAttackControl = this.add.text(700, 470, 'Set button',  { fontSize: 24 , color:'blue'});
+    this.crouchControlText = this.add.text(200, 560, 'Crouch(Down key)', { fontSize: 24 });
+    this.pauseControlText = this.add.text(200, 650, 'Pause(P)', { fontSize: 24 });
+    this.newRightControl = this.add.text(700, 200, 'Set',  { fontSize: 24, color:'blue'});
+    this.newLeftControl = this.add.text(700, 290, 'Set',   { fontSize: 24, color:'blue'});
+    this.newJumpControl = this.add.text(700, 380, 'Set',  { fontSize: 24,   color:'blue'});
+    this.newAttackControl = this.add.text(700, 470, 'Set',  { fontSize: 24 , color:'blue'});
+    this.newCrouchControl = this.add.text(700, 560, 'Set',  { fontSize: 24 , color:'blue'});
+    this.newPauseControl = this.add.text(700, 650, 'Set',  { fontSize: 24 , color:'blue'});
 
     this.newLeftControl.setInteractive().on('pointerdown', () => {
       this.rexUI.edit(this.newLeftControl);
@@ -27,7 +33,6 @@ export default class KeybindingScene extends Phaser.Scene {
 
     this.newRightControl.setInteractive().on('pointerdown', () => {
       this.rexUI.edit(this.newRightControl);
-
     })
 
     this.newJumpControl.setInteractive().on('pointerdown', () => {
@@ -37,6 +42,15 @@ export default class KeybindingScene extends Phaser.Scene {
     this.newAttackControl.setInteractive().on('pointerdown', () => {
       this.rexUI.edit(this.newAttackControl);
     })
+
+    this.newCrouchControl.setInteractive().on('pointerdown', () => {
+      this.rexUI.edit(this.newCrouchControl);
+    })
+
+    this.newPauseControl.setInteractive().on('pointerdown', () => {
+      this.rexUI.edit(this.newPauseControl);
+    })
+
 
   }
 
